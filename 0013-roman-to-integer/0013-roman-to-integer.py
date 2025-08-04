1ddd60c3990a8aa2e0000736b66695db1cc8c0c3
Islam <151960312+islamyasin07@users.noreply.github.com>
2025-08-04 11:30:50 +0300

    Time: 3 ms (78.76%), Space: 17.7 MB (89.97%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,10 +1,10 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         total = 0
         old_value = 0
         for char in reversed(s):
-            current_value = romans[char]
+            current_value = roman_map[char]
             if current_value < old_value:
                 total -= current_value
             else:
