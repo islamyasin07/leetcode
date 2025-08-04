b0e3dde0d73b36f0535b001a0690f8ccd42aa196
Islam <151960312+islamyasin07@users.noreply.github.com>
2025-08-04 12:02:51 +0300

    Time: 26 ms (90.08%), Space: 26.9 MB (32.21%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,12 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        min_price = float('inf')  
-        max_profit = 0           
+    def maxProfit(self, prices: list[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
 
         for price in prices:
             if price < min_price:
-                min_price = price  
-            else:
-                profit = price - min_price 
-                if profit > max_profit:
-                    max_profit = profit    
+                min_price = price  # نشتري بسعر أقل
+            elif price - min_price > max_profit:
+                max_profit = price - min_price  # نبيع بأعلى ربح
 
         return max_profit
